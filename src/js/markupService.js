eb5e1d5774cfab7d7ed86f9b2dd47a8fb35ce284
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-06 00:43:45 +0300

    Update markupService.js

--- src/js/markupService.js
+++ src/js/markupService.js
@@ -2,21 +2,22 @@ export function createCardsImagesMarkup(hits) {
     return hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
         return `
         <div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-    <div class="info">
-    <p class="info-item">
-      <b>Likes: ${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views: ${views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments: ${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads: ${downloads}</b>
-    </p>
-    </div>
-    </div>`
-    }).join('');
+        <a href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" class="img-small" loading="lazy"/>
+        </a>
+        <div class="info">
+        <p class="info-item">
+        <b>Likes: ${likes}</b>
+        </p>
+        <p class="info-item">
+        <b>Views: ${views}</b>
+        </p>
+        <p class="info-item">
+        <b>Comments: ${comments}</b>
+        </p>
+        <p class="info-item">
+        <b>Downloads: ${downloads}</b>
+        </p>
+        </div>
+        </div>`}).join('');
 }
