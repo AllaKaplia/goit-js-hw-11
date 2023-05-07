8780b36e8d84fb34b60c0de08021f1d4dc880059
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-07 23:23:50 +0300

    Update searchClassService.js

--- src/js/searchClassService.js
+++ src/js/searchClassService.js
@@ -9,15 +9,15 @@ export default class PixabaySearchService {
     async fetchImages() {
         return await axios.get(`https://pixabay.com/api/?key=${this.KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
             .then(response => {
-                this.incrementPage();
                 return response;
+            })
+            .then(data =>
+                {this.page += 1;
+                    return data;
             })
             .catch(error => {
                 console.error(error);
             });
-    }
-    incrementPage() {
-        this.page += 1;
     }
     resetPage() {
         this.page = 1;
