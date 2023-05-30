3dbb9c2f3b5688a03178bb6f9358eea5d5aa0bed
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-30 20:35:17 +0300

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,6 @@
 export default {
     searchForm: document.querySelector('.search-form'),
     galleryImages: document.querySelector('.gallery'),
-    photoCard: document.querySelector('.photo-card')
+    photoCard: document.querySelector('.photo-card'),
+    scrollUp: document.querySelector('.scroll-up')
 }
